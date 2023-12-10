3d0260fe469b8f8b54203966e86a71a6681b47f7
DeimisM <deimis.mardosas@outlook.com>
2023-12-10 18:07:36 +0200

    buying and fixes

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -4,8 +4,41 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    public float shrinkSpeed;
+    public Vector3 rotateSpeed;
+    public AudioSource source;
+
+    public GameObject particle;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime);
+
+        if (transform.localScale.x > 1)
+        {
+            transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+        }
+    }
+
     private void OnMouseDown()
     {
         GameManager.clicks++;
+
+        transform.localScale = Vector3.one * 1.4f;
+
+        source.PlayOneShot(source.clip);
+
+        Particles(2f);
+    }
+
+    void Particles(float distance)
+    {
+        var offset = Random.insideUnitSphere * distance;
+        Instantiate(particle, transform.position + offset, transform.rotation);
     }
 }
--- Assets/Worker.cs
+++ Assets/Worker.cs
@@ -1,14 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Worker : MonoBehaviour
 {
+    public string name;
+
     public int count;
     public int cost;
+    public int cps = 1;
+
+    public TMP_Text countText;
+    public TMP_Text priceText;
+
+    public Button button;
+
+    private void Start()
+    {
+        Load();
+        InvokeRepeating("Work", 1f, 1f);
+
+        countText.text = count.ToString();
+        priceText.text = "price:" + cost;
+    }
+
+    private void Update()
+    {
+        var canBuy = GameManager.clicks >= cost;
+
+        button.interactable = canBuy;
+    }
 
     public void Buy()
     {
+        if (GameManager.clicks < cost) return;
+
         count++;
+        GameManager.clicks -= cost;
+        cost = (int)(cost * 1.2f);
+
+        countText.text = count.ToString();
+        priceText.text = "price:" + cost;
+
+        Save();
+    }
+
+    void Work()
+    {
+        GameManager.clicks += count * cps;
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt(name + "count", count);
+        PlayerPrefs.SetInt(name + "cost", cost);
+    }
+
+    void Load()
+    {
+        count = PlayerPrefs.GetInt(name + "count");
+        cost = PlayerPrefs.GetInt(name + "cost", cost);       // default value antras
     }
 }
